8a4a49469f6e27dd28627ace5e551c23bb4ef029
VALEN-PC\arion <valentingr080@gmail.com>
2023-11-11 11:05:32 +0100

    API v1

--- conn.mjs
+++ conn.mjs
@@ -1,14 +1,14 @@
 import { MongoClient } from "mongodb";
-import 'dotenv/config';
+import "dotenv/config";
 
 const connectionString = process.env.CONNECTION_STRING;
 const client = new MongoClient(connectionString);
 let conn;
 try {
-    conn = await client.connect();
+  conn = await client.connect();
 } catch (e) {
-    console.error(e);
+  console.error(e);
 }
-let db = conn.db("el-rastro");
+let db = conn.db("web");
 let bids = db.collection("bids");
 export default bids;
