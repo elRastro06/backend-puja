62f0ff8d2f8073d49ae7dcf641f0d01880a69717
artachojf <p.artacho@uma.es>
2023-12-12 13:03:02 +0100

    Token verification on every call

--- index.mjs
+++ index.mjs
@@ -1,6 +1,7 @@
 import express from "express";
 import v1 from "./v1.mjs";
 import cors from "cors";
+import axios from "axios";
 
 const app = express();
 app.use(cors());
@@ -13,4 +14,15 @@ app.listen(port, () => {
     console.log(`Now listening on port ${port}`);
 });
 
-app.use("/v1", v1);
+const clients = process.env.CLIENTS != undefined ? process.env.CLIENTS : "localhost";
+
+const verifyToken = async (req, res, next) => {
+  try {
+    const response = await axios.get(`http://${clients}:5000/checkToken/${req.headers.authorization}`);
+    next();
+  } catch {
+    res.status(401).send({ error: "Invalid token" });
+  }
+}
+
+app.use("/v1", verifyToken, v1);
